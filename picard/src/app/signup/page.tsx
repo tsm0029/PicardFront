665357a2997f1623eeb83542319d7cd976216594
Tm <tsm0029@mix.wvu.edu>
2023-09-04 18:58:43 -0400

    routing test

--- picard/src/app/signup/page.tsx
+++ picard/src/app/signup/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 import Image from 'next/image'
 import React  from 'react';
-import Menu from "./components/menu";
+
 import { ChangeEvent, useState } from "react";
-import Link from 'next/link';
 
 // style={{
           
@@ -51,19 +50,13 @@ export default function Home() {
           background: 'black',
         }}></input> 
         </p>
-        <div>
-        <Link href="/signup">
-        <button  style={{
-          background: 'dark-grey',
-        }}> 
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-           Sign up</p></button> </Link>
-           <button  style={{
+        
+        <button onClick={sayHello} style={{
           background: 'dark-grey',
         }}>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-           Log in</p></button> </div>
-      {/* </div>  */}
+           Sign up</p></button> 
+           
 
        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
        
