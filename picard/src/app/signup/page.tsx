3568af19c45070e88110a9abdabc6060625c9c6a
Tm <tsm0029@mix.wvu.edu>
2023-09-05 11:41:57 -0400

    password made to not show text

--- picard/src/app/signup/page.tsx
+++ picard/src/app/signup/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
         </input>
         </p>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <input type="text" color='text-gray-950' id="password" placeholder='password' style={{
+          <input type="password" color='text-gray-950' id="password" placeholder='password' style={{
           background: 'black',
         }}></input> 
         </p>
