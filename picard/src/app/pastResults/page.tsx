d504fe750c38eac8ed9bd70faf925493da1bea61
ZackWildasin <zmw0005@mix.wvu.edu>
2023-09-08 15:30:33 -0400

    css stuff

---
+++ picard/src/app/pastResults/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React  from 'react';
+import Link from 'next/link';
+import "./page.css";
+
+type ContainerProps = {
+    styles: React.CSSProperties
+}
+
+
+export default function Results() {
+    return (
+        <div>
+
+        </div>
+    )
+}
