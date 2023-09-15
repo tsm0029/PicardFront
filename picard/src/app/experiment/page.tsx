e9520114e9ca7422a5d6a5fa31364e855d2e2994
ZackWildasin <zmw0005@mix.wvu.edu>
2023-09-15 15:43:48 -0400

    experiment and results pages

--- picard/src/app/experiment/page.tsx
+++ picard/src/app/experiment/page.tsx
@@ -5,82 +5,135 @@ import Link from 'next/link';
 import { ChangeEvent, useState } from "react";
 import "./page.css";
 
-// style={{
-          
-//   background: 'black',
-// }}>
-
 function sayHello() {
   alert('Loading');
 }
 
-
-
 export default function Home() {
-  
   return (
-    
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex">
+    <main className="main-container">
+
+      <div className="max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex">
+        <Link href="/homepage">
+          <button className="logo-button">
+            <Image src="/logo.png" width={100} height={100} alt="" />
+          </button>
+        </Link>
+
+        <Link href="/homepage">
+            <button
+              style={{
+                background: 'dark-grey',
+              }}
+            >
+              <p className="my-element">
+                Return to Main
+              </p>
+            </button>
+          </Link>
+      </div>
+
+      <div>
+        <div style={{position:"absolute", float: "left", left:525, top:100}}>
+          <button
+              style={{
+                  background: 'dark-grey',
+                  width: 350,
+                  marginRight: 25,
+              }}
+          >
+              <p className="my-element">
+                  Environment
+              </p>
+          </button>
+          <button
+              style={{
+                  background: 'dark-grey',
+              }}
+          >
+              <p className="my-element">
+                  Distribute
+              </p>
+          </button>
+        </div>
+      </div>
+
+      <div style={{position:"absolute", float: "left", left:525, top:190}}>
+        <button
+          style={{
+            background: 'dark-grey',
+            width: 275,
+          }}
+        >
+          <p className="my-element">
+            Select Algorithm
+          </p>
+        </button>
+      </div>  
         
-      <Link href="/homepage">
-           <button  style={{
-          background: 'dark-grey',
-        }}>
-          
-         <Image
-      src="/logo.png"
-      width={100}
-      height={100}
-      alt=""
-    /></button> </Link>
-        <div>
-        <button className="buttons" onClick={sayHello} style={{
-          background: 'dark-grey',
-        }}>
-        <p className="fixed right-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-           Environment</p></button> 
-           <button className="buttons" onClick={sayHello} style={{
-          background: 'dark-grey',
-        }}>
-        <p className="fixed right-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-           Distribute</p></button> </div>
-       
-        
-        
-        
-        
-         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        
-        </div> 
-        
+      <div style={{position:"absolute", float: "left", left:525, top:280}}>
+        <button
+          style={{
+            background: 'dark-grey',
+            width: 250,
+          }}
+        >
+          <p className="my-element">
+            Number of Nodes
+          </p>
+        </button>
+      </div>
+
+      <div style={{position:"absolute", float: "left", left:800, top:370}}>
+        <button
+          style={{
+            background: 'dark-grey',
+            width: 160,
+          }}
+        >
+          <p className="my-element">
+            Run Experiment
+          </p>
+        </button>
+      </div>
+
+      <div >
+        <div style={{position:"absolute", float: "left", left:1200, top:500}}>
+          <button
+            style={{
+              background: 'dark-grey',
+              width: 160,
+            }}
+          >
+            <p className="my-element">
+              Save Locally
+            </p>
+          </button>
+        </div>
+
+        <div style={{position:"absolute", float: "left", left:1200, top:650}}>
+          <button
+            style={{
+              background: 'dark-grey',
+              width: 160,
+            }}
+          >
+            <p className="my-element">
+              Delete Results
+            </p>
+          </button>
+        </div>
         
       </div>
 
+      <div 
+        className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      </div>
 
-        <div className="relative flex place-items-center before:absolute before:h-[100px] before:w-[300px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        </div>
-        <button onClick={sayHello} style={{
-          background: 'dark-grey',
-        }}>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-           Select Algorithm</p></button> 
-           <button onClick={sayHello} style={{
-          background: 'dark-grey',
-        }}>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-           Number of Nodes</p></button> 
-        
-        <button onClick={sayHello} style={{
-          background: 'dark-grey',
-        }}>
-       
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-           Run Experiment</p></button> 
-       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-       
-      </div> 
+      <div 
+        className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      </div>
+
     </main>
-  )
-    }
-  
+  );
+}
--- picard/src/app/pastResults/page.tsx
+++ picard/src/app/pastResults/page.tsx
@@ -3,15 +3,156 @@ import React  from 'react';
 import Link from 'next/link';
 import "./page.css";
 
-type ContainerProps = {
-    styles: React.CSSProperties
-}
-
-
 export default function Results() {
     return (
-        <div>
+        <main className="main-container">
 
-        </div>
+            <div className="max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex">
+                <Link href="/homepage">
+                <button className="logo-button">
+                    <Image src="/logo.png" width={100} height={100} alt="" />
+                </button>
+                </Link>
+
+                <Link href="/homepage">
+                    <button
+                    style={{
+                        background: 'dark-grey',
+                    }}
+                    >
+                    <p className="my-element">
+                        Return to Main
+                    </p>
+                    </button>
+                </Link>
+            </div>
+
+            <div>
+                <div style={{position:"absolute", float: "left", left:550, top:200}}>
+                    <button
+                        style={{
+                            background: 'dark-grey',
+                            width: 500,
+                            marginRight: 15,
+                        }}
+                    >
+                        <p className="my-element">
+                            Experiment 1: Results and Metrics
+                        </p>
+                    </button>
+                    <button
+                        style={{
+                            background: 'dark-grey',
+                        }}
+                    >
+                        <p className="my-element">
+                            Delete
+                        </p>
+                    </button>
+                </div>
+                
+                <div style={{position:"absolute", float: "left", left:550, top:300}}>
+                    <button
+                    style={{
+                        background: 'dark-grey',
+                        width: 500,
+                        marginRight: 15,
+                    }}
+                    >
+                        <p className="my-element">
+                            Experiment 2: Results and Metrics
+                        </p>
+                    </button>
+                    <button
+                        style={{
+                            background: 'dark-grey',
+                        }}
+                    >
+                        <p className="my-element">
+                            Delete
+                        </p>
+                    </button>
+                </div>
+
+                <div style={{position:"absolute", float: "left", left:550, top:400}}>
+                    <button
+                    style={{
+                        background: 'dark-grey',
+                        width: 500,
+                        marginRight: 15,
+                    }}
+                    >
+                        <p className="my-element">
+                            Experiment 3: Results and Metrics
+                        </p>
+                    </button>
+                    <button
+                        style={{
+                            background: 'dark-grey',
+                        }}
+                    >
+                        <p className="my-element">
+                            Delete
+                        </p>
+                    </button>
+                </div>
+
+                <div style={{position:"absolute", float: "left", left:550, top:500}}>
+                    <button
+                    style={{
+                        background: 'dark-grey',
+                        width: 500,
+                        marginRight: 15,
+                    }}
+                    >
+                        <p className="my-element">
+                            Experiment 4: Results and Metrics
+                        </p>
+                    </button>
+                    <button
+                        style={{
+                            background: 'dark-grey',
+                        }}
+                    >
+                        <p className="my-element">
+                            Delete
+                        </p>
+                    </button>
+                </div>
+
+                <div style={{position:"absolute", float: "left", left:550, top:600}}>
+                    <button
+                    style={{
+                        background: 'dark-grey',
+                        width: 500,
+                        marginRight: 15,
+                    }}
+                    >
+                        <p className="my-element">
+                            Experiment 5: Results and Metrics
+                        </p>
+                    </button>
+                    <button
+                        style={{
+                            background: 'dark-grey',
+                        }}
+                    >
+                        <p className="my-element">
+                            Delete
+                        </p>
+                    </button>
+                </div>
+                                
+            </div>
+
+            <div 
+                className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+            </div>
+
+            <div 
+                className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            </div>
+
+        </main>
     )
 }
