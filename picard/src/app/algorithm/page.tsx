fd87259cdcba953e31fa815fd832231f3c6d780b
Tm <tsm0029@mix.wvu.edu>
2023-09-15 15:58:36 -0400

    fixed some css spacing

--- picard/src/app/algorithm/page.tsx
+++ picard/src/app/algorithm/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import React  from 'react';
 import Link from 'next/link';
 import { ChangeEvent, useState } from "react";
-
+import './algor.css'; // Importing a CSS file
 
 function sayHello() {
   alert('Loading');
@@ -15,8 +15,8 @@ export default function Home() {
   
   return (
     
-    <main className="flex min-h-screen flex-col  justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-right justify-between font-mono text-lg lg:flex">
+    <main className="">
+      <ul className="logo-button">
          {/* The following code shows the logo in the top left corner and takes you back to the login page when clicked*/}
       <Link href="/homepage">
            <button  style={{
@@ -29,36 +29,33 @@ export default function Home() {
       alt=""
       
     /></button> </Link>
-        <div>
-         </div>
-         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        
-        </div> 
-        
-        
-      </div>
+      </ul>
+      <div className='spacing'></div>
+      <div className='spacing'></div>
 
-
-        <div className="relative flex place-items-center before:absolute before:h-[100px] before:w-[300px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className="justify-content:right place-items-center before:absolute before:h-[100px] before:w-[300px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         </div>
+        <div className='spacing'></div>
         {/* the following code makes the algorithm 1 button that currently just displays a loading message */}
         <button onClick={sayHello} style={{
           background: 'dark-grey',
         }}>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="algo">
         Algorithm 1</p></button> 
            <button onClick={sayHello} style={{
           background: 'dark-grey',
         }}>
+          <div className='spacing'></div>
           {/* the following code makes the algorithm 2 button that currently just displays a loading message */}
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <p className="algo">
         Algorithm 2</p></button> 
         
         <button onClick={sayHello} style={{
           background: 'dark-grey',
-        }}>
+        }}> 
+        <div className='spacing'></div>
        {/* the following code makes the algorithm 3 button that currently just displays a loading message */}
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+       <p className="algo">
         Algorithm 3</p></button> 
        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
        
--- picard/src/app/page.tsx
+++ picard/src/app/page.tsx
@@ -4,6 +4,7 @@ import React  from 'react';
 import Menu from "./components/menu";
 import { ChangeEvent, useState } from "react";
 import Link from 'next/link';
+import './styles.css';
 
 // style={{
           
@@ -54,14 +55,14 @@ export default function Home() {
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         </div>
         {/* The following code makes the input field for the email*/}
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 rows:10">
+        <p className="input1">
           <input type="text"  id="email" placeholder='email' style={{
           background: 'black',
         }}>
         </input>
         </p>
         {/* The following code makes the input field for the password*/}
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="input1">
           <input type="password" color='text-gray-950' id="password" placeholder='password' style={{
           background: 'black',
         }}></input> 
@@ -72,14 +73,14 @@ export default function Home() {
         <button  style={{
           background: 'dark-grey',
         }}> 
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="my-element">
            Sign up</p></button> </Link>
            {/* The following code makes the login button that will take you to the homepage*/}
            <Link href="/homepage">
            <button  style={{
           background: 'dark-grey',
         }}>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="my-element">
            Log in</p></button> </Link> </div>
       
 
