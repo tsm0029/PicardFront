becf17af229c8268cd34f80c97ec9d11bc504005
ZackWildasin <zmw0005@mix.wvu.edu>
2023-10-17 13:31:08 -0400

    configured project for firebase

---
+++ picard/src/app/api/router.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwOhpJ8PpM-_1-AvxAbKHGZL5qyuxQMk0",
+  authDomain: "picard-8ec76.firebaseapp.com",
+  projectId: "picard-8ec76",
+  storageBucket: "picard-8ec76.appspot.com",
+  messagingSenderId: "1060937711233",
+  appId: "1:1060937711233:web:4d91949ad96b47e8c99780"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
